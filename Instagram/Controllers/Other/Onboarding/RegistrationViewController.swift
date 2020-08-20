528a1822eb0ba29023871bf7605e3036f90cc86f
Ilya <ego.voropaev@gmail.com>
2020-08-20 20:24:17 +0300

    Created login screen;

--- Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -13,7 +13,7 @@ class RegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
 
 }
---
+++ Instagram/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Илья on 20.08.2020.
+//  Copyright © 2020 Ilya. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
